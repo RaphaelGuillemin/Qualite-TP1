05c441e86131b5d98dc46705162dec2f25f3dd76
raphaelguillemin <raphael.guillemin@umontreal.ca>
2020-09-15 17:32:54 -0400

    Added scanner and test file

--- src/Parseur.java
+++ src/Parseur.java
@@ -1,5 +1,135 @@
-public class Parseur {
-    public static void main(String[] args) throws Exception {
+/* @Author Laura Bégin ()
+ * @Author Raphaël Guillemin (p1202392, 20129638)
+ */
 
+import java.io.File;
+import java.util.Scanner;
+
+public class Parseur {
+    // Nombre de lignes de code d’une classe
+    private int classe_LOC;
+    // Nombre de lignes de code d’une méthode
+    private int methode_LOC;
+    // Nombre de lignes de code d’une classe qui contiennent des commentaires
+    private int classe_CLOC;
+    // Nombre de lignes de code d’une méthode qui contiennent des commentaires
+    private int methode_CLOC;
+    // Densité de commentaires pour une classe : classe_DC = classe_CLOC / classe_LOC
+    private int classe_DC;
+    // Densité de commentaires pour une méthode : methode_DC = methode_CLOC / methode_LOC
+    private int methode_DC;
+
+
+    //TODO REMOVE THIS BEFORE SENDING
+    public static void print(String str){
+        System.out.println(str);
+    }
+    /*
+     * @returns classe_LOC
+     */
+    public int getClasse_LOC() {
+        return classe_LOC;
+    }
+
+    /*
+     * @param classe_LOC
+     */
+    public void setClasse_LOC(int classe_LOC) {
+        this.classe_LOC = classe_LOC;
+    }
+
+    /*
+     * @returns methode_LOC
+     */
+    public int getMethode_LOC() {
+        return methode_LOC;
+    }
+
+    /*
+     * @param methode_LOC
+     */
+    public void setMethode_LOC(int methode_LOC) {
+        this.methode_LOC = methode_LOC;
+    }
+
+    /*
+     * @returns classe_CLOC
+     */
+    public int getClasse_CLOC() {
+        return classe_CLOC;
+    }
+
+    /*
+     * @param classe_CLOC
+     */
+    public void setClasse_CLOC(int classe_CLOC) {
+        this.classe_CLOC = classe_CLOC;
+    }
+
+    /*
+     * @returns methode_CLOC
+     */
+    public int getMethode_CLOC() {
+        return methode_CLOC;
+    }
+
+    /*
+     * @param methode_CLOC
+     */
+    public void setMethode_CLOC(int methode_CLOC) {
+        this.methode_CLOC = methode_CLOC;
+    }
+
+    /*
+     * @returns classe_DC
+     */
+    public int getClasse_DC() {
+        return classe_DC;
+    }
+
+    /*
+     * @param classe_DC
+     */
+    public void setClasse_DC(int classe_DC) {
+        this.classe_DC = classe_DC;
+    }
+
+    /*
+     * @returns methode_DC
+     */
+    public int getMethode_DC() {
+        return methode_DC;
+    }
+
+    /*
+     * @param methode_DC
+     */
+    public void setMethode_DC(int methode_DC) {
+        this.methode_DC = methode_DC;
+    }
+
+    /*
+     * @param args A string of the path to the java file to read
+     */
+    public static void main(String[] args) throws Exception {
+        String filePath;
+        if (args.length != 0){
+            filePath = args[0];
+        } else {
+            System.err.println("Veuillez ajouter le chemin vers le fichier java désiré.");
+            return;
+        }
+
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
---
+++ tests/TicTacToeGUI.java
@@ -0,0 +1,147 @@
+import java.io.*;
+import java.net.Socket;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class TicTacToeGUI extends Application {
+
+    private Text status;
+    private String lettre;
+    private Button bouton;
+
+    public static void main(String[] args) {
+        TicTacToeGUI.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Ouverture du Socket qui se connecte au serveur de TicTacToe
+        Socket clientSocket = new Socket("127.0.0.1", 1337);
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                clientSocket.getOutputStream()
+        ));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                clientSocket.getInputStream()
+        ));
+
+        /**
+         * Notez qu'on peut envoyer des messages au serveur en utilisant :
+         * 
+         * writer.append("mon message" + "\n");
+         * writer.flush();
+         */
+
+        // Création de l'interface graphique
+        VBox root = new VBox();
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setResizable(false);
+        HBox ligne = new HBox();
+        ligne.setMaxHeight(24);
+        GridPane grille = new GridPane();
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                Button bouton = new Button("");
+                bouton.setMinSize(100,92);
+                bouton.setId((i+1)+":"+(j+1));
+                grille.add(bouton,i,j);
+                bouton.setOnAction((event) -> {
+                    try {
+                        writer.append(bouton.getId());
+                        writer.newLine();
+                        writer.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
+        root.getChildren().add(grille);
+
+        // Ajout de la barre de status
+
+        status = new Text();
+        ligne.getChildren().add(status);
+        root.getChildren().add(ligne);
+
+        /**
+         * Crée un thread qui écoute les messages envoyés par le serveur et qui
+         * met à jour l'interface graphique en conséquence
+         */
+        Thread listener = new Thread(() -> {
+            try {
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    setStatus("");
+                    if (this.lettre==null){
+                        this.lettre = line.split(" ")[1];
+                        if(this.lettre.equals("x")){
+                            setStatus("Commencez");
+                        } else {
+                            setStatus("Les X commencent");
+                        }
+                    } else if (line.equals("tie")){
+                        setStatus("Match nul");
+                    } else if (line.charAt(0)=='x'||line.charAt(0)=='o'){
+                        if (lettre.charAt(0)==line.charAt(0)){
+                            setStatus("Vous avez gagné!");
+                        } else {
+                            setStatus("Vous avez perdu...");
+                        }
+                    } else if (line.split(" ")[0].equals("move")){
+                        String[] ligneString = line.split(" ");
+                        for (Node node : grille.getChildren()) {
+                            if (node.getId().equals(ligneString[2])) {
+                                bouton = (Button)node;
+                                setTextButton(ligneString[1]);
+                                break;
+                            }
+                        }
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        listener.start();
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Tic Tac Toe");
+        // Fermer le programme (tous les threads) lorsqu'on ferme la fenêtre
+        primaryStage.setOnCloseRequest((event) -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        primaryStage.show();
+    }
+
+    private void setTextButton(String str){
+        Platform.runLater(() -> {
+          if (this.bouton.getText().equals("")) {
+            this.bouton.setText(str);
+          }
+        });
+
+    }
+
+    private void setStatus(String str) {
+        /**
+         * Important : toutes les modifications de l'interface graphique
+         * **doivent** se faire sur le Thread d'application de JavaFX
+         *
+         * Si un autre thread souhaite modifier l'interface, on doit passer par
+         * la méthode Platform.runLater(runnable);
+         */
+        Platform.runLater(() -> {
+            status.setText(str);
+        });
+    }
+}
