87573f437db69dedce147debc36761809797bbfa
Laura Bégin <laurabegin1@gmail.com>
2020-10-09 18:09:05 -0400

    fix tests after parser refactoring

--- test/ParserTest.java
+++ test/ParserTest.java
@@ -14,18 +14,18 @@ public class ParserTest {
         JavaFile testJavaFile = Parser.parseNewFile(testFile);
 
         // Assert
-        assert testJavaFile.getPath().equals(filePath);
+        assert testJavaFile.getPath().equals(folderPath + filePath);
     }
 
     public static void parseMainFileTest() throws Exception {
         // Prepare
         String filePath = "/testfolder/Main.java";
         String className = "Main";
-        int classLOC = 40;
-        int classCLOC = 25;
-        double classDC = 0.625;
+        int classLOC = 43;
+        int classCLOC = 26;
+        double classDC = 0.60465115;
         double classWMC = 8;
-        double classBC = 0.078125;
+        double classBC = 0.075581394;
         File testFile = new File(folderPath + filePath);
 
         // Run
@@ -37,9 +37,9 @@ public class ParserTest {
         assert testClass.getName().equals(className);
         assert testClass.getClasse_LOC() == classLOC;
         assert testClass.getClasse_CLOC() == classCLOC;
-        assert testClass.getClasse_DC() == classDC;
+        assert testClass.getClasse_DC() == (float) classDC;
         assert testClass.getWMC() == classWMC;
-        assert testClass.getClasse_BC() == classBC;
+        assert testClass.getClasse_BC() == (float) classBC;
     }
 
     public static void parseInterfaceTest() throws Exception {
@@ -48,9 +48,9 @@ public class ParserTest {
         File testFile = new File(folderPath + filePath);
         String interfaceName = "Animal";
         String className = "Pig";
-        int interfaceLOC = 2;
+        int interfaceLOC = 4;
         int interfaceCLOC = 3;
-        double interfaceDC = 1.5;
+        double interfaceDC = 0.75;
         double interfaceWMC = 0;
 
 
@@ -64,12 +64,12 @@ public class ParserTest {
         assert firstClass.getName().equals(interfaceName);
         assert SecondClass.getName().equals(className);
 
-        assert firstClass.getName().equals(className);
+        assert firstClass.getName().equals(interfaceName);
         assert firstClass.getClasse_LOC() == interfaceLOC;
         assert firstClass.getClasse_CLOC() == interfaceCLOC;
         assert firstClass.getClasse_DC() == interfaceDC;
         assert firstClass.getWMC() == interfaceWMC;
-        assert Float.isFinite(firstClass.getClasse_BC());
+        assert Float.isInfinite(firstClass.getClasse_BC());
     }
 
     public static void parseMethodsTest() throws Exception {
